0a7ab9167b18b8642c4698a12396f92ca4ea77b5
Rob Calcavecchia <robbkore@gmail.com>
2023-06-01 15:44:07 -0400

    Init

---
+++ config/bundles.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    Pimcore\Bundle\ApplicationLoggerBundle\PimcoreApplicationLoggerBundle::class => ['all' => true],
+    Pimcore\Bundle\SimpleBackendSearchBundle\PimcoreSimpleBackendSearchBundle::class => ['all' => true],
+    Pimcore\Bundle\TinymceBundle\PimcoreTinymceBundle::class => ['all' => true],
+    Pimcore\Bundle\UuidBundle\PimcoreUuidBundle::class => ['all' => true],
+];
