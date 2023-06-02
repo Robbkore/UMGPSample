3c75b981471471a476448024c00c4edf14650e1b
Rob Calcavecchia <robbkore@gmail.com>
2023-06-02 18:45:27 -0400

    feat: attach ProductUpdateListener and process changes if the Product object is being updated
    The ProductUpdateListener detects changes to the Product object and has an added check to make sure the Product is published.
    
    Normally, I would have asked what parameters would make us want to create, update and either disable or something when a Product goes from a state of Published to Unpublished, but this is small-scale, so I'm hopeful this is acceptable for that purpose.
    
    If anyone is reading this, kudos to you.

--- bundles/Robbkore/ProductExporterBundle/src/Listener/ProductUpdateListener.php
+++ bundles/Robbkore/ProductExporterBundle/src/Listener/ProductUpdateListener.php
@@ -2,8 +2,10 @@
 
 namespace Robbkore\ProductExporterBundle\Listener;
 
+use Pimcore\Event\Model\DataObjectEvent;
 use Pimcore\Logger;
 use Pimcore\Event\Model\ElementEventInterface;
+use Pimcore\Model\DataObject\Product;
 
 class ProductUpdateListener {
     /**
@@ -11,6 +13,28 @@ class ProductUpdateListener {
      */
     public function onObjectPostUpdate(ElementEventInterface $eventElement): void {
         $logger = new Logger();
-        $logger->info('ProductUpdateListener::onObjectPostUpdate');
+        $logger->info('ProductUpdateListener::onObjectPostUpdate called');
+
+        if (!($eventElement instanceof DataObjectEvent)) {
+            $logger->error('ProductUpdateListener: This listener can only be bound to DataObjectEvents. Please check your service ');
+            return;
+        }
+
+        $dataObject = $eventElement->getObject();
+
+        if ($dataObject instanceof Product) {
+            $sku = $dataObject->getSku();
+            $name = $dataObject->getName();
+            $price = $dataObject->getPrice();
+            $media_type = $dataObject->getMedia_type();
+            $isPublished = $dataObject->isPublished();
+
+            if (!$isPublished) {
+                $logger->info('ProductUpdateListener: Product is not published.');
+                return;
+            }
+
+            $logger->info('ProductUpdateListener: Product Detected: ' . $name . '(' . $sku . ')  ' . $price . ' ' . $media_type);
+        }
     }
 }
